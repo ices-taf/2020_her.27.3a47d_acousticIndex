83b8f2df971fc361783977665f02287cd8276b1b
Benoit Berges <benoit.berges@gmail.com>
2020-06-19 12:14:29 +0200

    update code. Testing writing taf table

--- model.R
+++ model.R
@@ -3,7 +3,7 @@
 library(icesTAF)
 taf.library(Rstox)
 taf.library(mgcv)
-library(Rstox)
+#library(Rstox)
 
 data_mapping <- read.csv("bootstrap/data/data_mapping.csv")
 
@@ -18,6 +18,24 @@ for(idxYear in project_years){
   
   currentProjects <- as.character(unique(data_mapping[data_mapping$year == idxYear,]$project))
   for(idxProject in currentProjects){
+    reopenProject(file.path(getwd(),idxProject))
     runBaseline(projectName = file.path(getwd(),idxProject), save = TRUE,exportCSV = TRUE,modelType = c('baseline'))
+    runBaseline(projectName = file.path(getwd(),idxProject), save = TRUE,exportCSV = TRUE,modelType = c('baseline-report'))
+    
+    currentBaseLineReport <- getBaseline(file.path(getwd(),idxProject),save = FALSE,exportCSV = FALSE,modelType = c('baseline-report'))
+    
+    write.taf(as.data.frame(currentBaseLineReport$outputData$FillMissingData))
+    
+    # StoX bootstraping
+    #runBootstrap(projectName=file.path(getwd(),idxProject),
+    #             bootstrapMethod="AcousticTrawl",
+    #             acousticMethod="PSU~Stratum",
+    #             bioticMethod="PSU~Stratum",
+    #             startProcess="TotalLengthDist",
+    #             endProcess="SuperIndAbundance",
+    #             nboot=nboot,
+    #             seed=1, cores=4)
+    
+    #saveProjectData(dataDirs[idxDataDir])
   }
 }
