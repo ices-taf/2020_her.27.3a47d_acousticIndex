ef4d95d86e64cdaaf421264d9a693ab3499338d9
Colin Millar <colin.millar@ices.dk>
2020-06-22 11:13:53 +0200

    no need to install these - already on the server

--- bootstrap/install_rstox.R
+++ bootstrap/install_rstox.R
@@ -1,16 +1,4 @@
 library(icesTAF)
 
 mkdir("../../library")
-
-dep.pck <- c(	"data.table", 
-				"ggplot2", 
-				"pbapply", 
-				"rgdal", 
-				"rgeos", 
-				"rJava", 
-				"sp", 
-				"XML",
-				"mgcv")
-install.packages(dep.pck, repos="http://cran.us.r-project.org", type="binary",lib="../../library")
-
 install.packages("ftp://ftp.imr.no/StoX/Download/Rstox/Rstox_1.11.tar.gz", repos=NULL,  lib="../../library")
