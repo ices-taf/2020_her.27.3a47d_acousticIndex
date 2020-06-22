c2bbc1920f304e5a8f5a5a26ed95784fc8963331
Colin Millar <colin.millar@ices.dk>
2020-06-22 12:20:24 +0200

    copy project from data folder

--- model.R
+++ model.R
@@ -1,39 +1,47 @@
 ## Run analysis, write model results
 
-library(icesTAF)
-taf.library(Rstox)
-taf.library(mgcv)
-#library(Rstox)
-options(java.parameters = "-Xmx2000m")
-library(rJava) 
+# run these set up options first
+options(java.parameters = "-Xmx2000m") # increase RAM for Java VM
+library(rJava)
 .jinit()
 
-data_mapping <- read.csv("bootstrap/data/data_mapping.csv")
+library(icesTAF)
+taf.library(Rstox)
+library(mgcv)
 
+# create model dir
+mkdir("model")
+
+# copy over data (create stucture for stox in model folder)
+cp(from = 'data/stox-project', to = 'model')
+
+
+data_mapping <- read.csv("bootstrap/data/data_mapping.csv")
 project_years <- unique(data_mapping$year)
 
-nboot <- 500
+# get configuration file
+for (idxYear in project_years) {
+  currentProjects <- as.character(unique(data_mapping[data_mapping$year == idxYear, ]$project))
 
-setwd('model')
+  for (idxProject in currentProjects) {
+    msg("running Baseline for: ", idxProject)
 
-for(idxYear in project_years){
-  setwd(as.character(idxYear))
-  
-  currentProjects <- as.character(unique(data_mapping[data_mapping$year == idxYear,]$project))
-  for(idxProject in currentProjects){
-    .jcall("java/lang/System", method = "gc") 
-    reopenProject(file.path(getwd(),idxProject))
-    runBaseline(projectName = file.path(getwd(),idxProject), save = TRUE,exportCSV = TRUE,modelType = c('baseline'))
-    runBaseline(projectName = file.path(getwd(),idxProject), save = TRUE,exportCSV = TRUE,modelType = c('baseline-report'))
-    
-    currentBaseLineReport <- getBaseline(file.path(getwd(),idxProject),save = FALSE,exportCSV = FALSE,modelType = c('baseline-report'))
-    
+    wkdir <- file.path(getwd(), "model", "stox-project", as.character(idxYear), as.character(idxProject))
+
+    openProject(wkdir)
+
+    runBaseline(projectName = wkdir, save = TRUE, exportCSV = TRUE, modelType = c("baseline"))
+    runBaseline(projectName = wkdir, save = TRUE, exportCSV = TRUE, modelType = c("baseline-report"))
+
+    currentBaseLineReport <- getBaseline(wkdir, save = FALSE, exportCSV = FALSE, modelType = c("baseline-report"))
+
     save(currentBaseLineReport,
-         file=file.path('../',paste0(idxProject,'_BaseLineReport.RData')),
-         compress="xz")
-    
+      file = file.path("model", paste0(idxProject, "_BaseLineReport.RData")),
+      compress = "xz"
+    )
+
     # StoX bootstraping
-    #runBootstrap(projectName=file.path(getwd(),idxProject),
+    # runBootstrap(projectName=file.path(getwd(),idxProject),
     #             bootstrapMethod="AcousticTrawl",
     #             acousticMethod="PSU~Stratum",
     #             bioticMethod="PSU~Stratum",
@@ -41,9 +49,10 @@ for(idxYear in project_years){
     #             endProcess="SuperIndAbundance",
     #             nboot=nboot,
     #             seed=1, cores=4)
-    
-    #saveProjectData(dataDirs[idxDataDir])
+
+    # saveProjectData(dataDirs[idxDataDir])
+
+    # garbage collect
+    .jcall("java/lang/System", method = "gc")
   }
 }
-
-setwd('../..')
