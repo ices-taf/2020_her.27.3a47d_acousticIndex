ea614af627cd4ae5bae39ff6122b9647b1205aa8
Colin Millar <colin.millar@ices.dk>
2020-06-22 12:32:13 +0200

    create sample output from model folder

--- output.R
+++ output.R
@@ -4,19 +4,20 @@ library(icesTAF)
 mkdir("output")
 
 data_mapping <- read.csv("bootstrap/data/data_mapping.csv")
-
-setwd('output')
+project_years <- unique(data_mapping$year)
 
 for(idxYear in project_years){
-  setwd(as.character(idxYear))
-  
   currentProjects <- as.character(unique(data_mapping[data_mapping$year == idxYear,]$project))
+
   for(idxProject in currentProjects){
-    load(file.path("../model",paste0(idxProject,'_BaseLineReport.RData')))
-    
-    write.taf(as.data.frame(currentBaseLineReport$outputData$FillMissingData),
-              "FillMissingData.csv")
+    msg("creating outout for: ", idxProject)
+    load(file.path("model", paste0(idxProject, "_BaseLineReport.RData")))
+
+    # test output creation
+    write.taf(
+      as.data.frame(currentBaseLineReport$outputData$FillMissingData),
+      file = paste0(idxProject, "FillMissingData.csv"),
+      dir = "output"
+    )
   }
 }
-
-setwd('..')
