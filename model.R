790cc56aa91940935bbc21b63cc8a90b8611fe1b
bberges <benoit.berges@wur.nl>
2020-11-17 23:57:10 +0100

    updated code to finish the calculation of the index and show some outputs

--- model.R
+++ model.R
@@ -7,6 +7,8 @@ library(rJava)
 
 library(icesTAF)
 taf.library(Rstox)
+taf.library(FLFleet)
+taf.library(FLCore)
 library(mgcv)
 
 # source functions
@@ -17,10 +19,17 @@ source('bootstrap/software/utilities/fill_missing_species_v3.R')
 # create model dir
 mkdir("model")
 
-# age parameters
+# parameters
+surveyYear <- 2019
+minYear <- surveyYear
+maxYear <- surveyYear
+yearVec <- minYear:maxYear
+nYears  <- length(yearVec)
 minAge  <- 0
 maxAge  <- 9
 ageVec  <- minAge:maxAge
+nAges   <- length(ageVec)
+nIter   <- 1
 
 
 # load NO split
@@ -32,7 +41,66 @@ cp(from = 'data/stox-project', to = 'model')
 # load project mapping
 project_mapping <- read.csv("bootstrap/data/project_mapping.csv")
 
+######################################################################
+######################################################################
+######################################################################
+#### # initialise FLStock objects
+######################################################################
+######################################################################
+######################################################################
+
+# stock object
+FLR_temp <- FLQuant( array(-1,dim = c(nAges,nYears,1,1,1,nIter)),
+                     dimnames=list( age=as.character(ageVec),
+                                    year=as.character(yearVec),
+                                    unit=c("1e6"),
+                                    season="all", 
+                                    area='all'),
+                     units='1e6')
+
+NSAS <- FLStock(name = 'NSAS', 
+                desc = 'NSAS',
+                FLR_temp)
+
+WBSS <- FLStock(name = 'WBSS', 
+                desc = 'WBSS',
+                FLR_temp)
+
+######################################################################
+#### # initialise FLIndex objects
+######################################################################
+
+# EU NO index object
+FLR_HERAS_EU_NO <- FLQuant( array(-1,dim = c(nAges,nYears,1,1,2,nIter)),
+                            dimnames=list(age=as.character(ageVec),
+                                          year=as.character(yearVec),
+                                          unit=c("1e6"),
+                                          season="all", 
+                                          area=c('EU','NO')),
+                            units='1e6')
+FLR_HERAS_EU_NO <- FLIndex(name = 'HERAS_EU_NO', 
+                           desc = 'HERAS index from EU and NO StoX projects', 
+                           index=FLR_HERAS_EU_NO)
+
+# total index object
+FLR_HERAS <- FLQuant( array(-1,dim = c(nAges,nYears,1,1,1,nIter)),
+                      dimnames=list(age=as.character(ageVec),
+                                    year=as.character(yearVec),
+                                    unit="1e6",
+                                    season="all", 
+                                    area='all'),
+                      units='1e6')
+FLR_HERAS <- FLIndex( name = 'HERAS_all', 
+                      desc = 'HERAS index (total)', 
+                      index=FLR_HERAS)
+
+HERAS.NSAS <- FLIndices(FLR_HERAS_EU_NO, FLR_HERAS)
+HERAS.WBSS <- FLIndices(FLR_HERAS_EU_NO, FLR_HERAS)
+
+######################################################################
 # run split projects
+######################################################################
+
 for(idxProject in 1:dim(project_mapping)[1]){
   msg("running Baseline for: ", project_mapping$project_name[idxProject])
   
@@ -64,7 +132,7 @@ for(idxProject in 1:dim(project_mapping)[1]){
   #             bioticMethod="PSU~Stratum",
   #             startProcess="TotalLengthDist",
   #             endProcess="SuperIndAbundance",
-  #             nboot=500,
+  #             nboot=nIter,
   #             seed=1, cores=4)
   
   #saveProjectData(wkdir)
@@ -75,8 +143,40 @@ for(idxProject in 1:dim(project_mapping)[1]){
 #################################################################
 ## create index and assign values
 #################################################################
-#outIndex <- compute_NSAS_WBSS_strata(   endTabEU,
-#                                        endTabNO,
-#                                        split_prop,
-#                                        ageVec,
-#                                        surveyYear)
+outIndex <- compute_NSAS_WBSS_strata(   endTabEU,
+                                        endTabNO,
+                                        split_prop,
+                                        ageVec,
+                                        surveyYear)
+
+# assign numbers at age to FLR objects
+HERAS.NSAS$HERAS_EU_NO@index[,ac(surveyYear),,,'NO'] <- colSums(outIndex$NSAS_NO)*1e-6
+HERAS.WBSS$HERAS_EU_NO@index[,ac(surveyYear),,,'NO'] <- colSums(outIndex$WBSS_NO)*1e-6
+HERAS.NSAS$HERAS_EU_NO@index[,ac(surveyYear),,,'EU'] <- colSums(outIndex$NSAS_EU)*1e-6
+HERAS.WBSS$HERAS_EU_NO@index[,ac(surveyYear),,,'EU'] <- colSums(outIndex$WBSS_EU)*1e-6
+
+# fill in stock weight at age and maturity
+NSAS@stock.wt[,ac(surveyYear)]  <- outIndex$wa$NSAS
+NSAS@mat[,ac(surveyYear)]       <- outIndex$propM$NSAS
+NSAS@stock.n[,ac(surveyYear)]   <- areaSums(HERAS.NSAS$HERAS_EU_NO@index[,ac(surveyYear)])
+
+WBSS@stock.wt[,ac(surveyYear)]  <- outIndex$wa$WBSS
+WBSS@mat[,ac(surveyYear)]       <- outIndex$propM$WBSS
+WBSS@stock.n[,ac(surveyYear)]   <- areaSums(HERAS.WBSS$HERAS_EU_NO@index[,ac(surveyYear)])
+
+# fill in SSB
+NSAS@stock[,ac(surveyYear)] <- sum(NSAS@stock.n[,ac(surveyYear)]*NSAS@stock.wt[,ac(surveyYear)]*NSAS@mat[,ac(surveyYear)])
+
+WBSS@stock[,ac(surveyYear)] <- sum(WBSS@stock.n[,ac(surveyYear)]*WBSS@stock.wt[,ac(surveyYear)]*WBSS@mat[,ac(surveyYear)])
+
+# combine NO and EU components
+HERAS.NSAS$HERAS_all@index[] <- areaSums(HERAS.NSAS$HERAS_EU_NO@index)
+HERAS.WBSS$HERAS_all@index[] <- areaSums(HERAS.WBSS$HERAS_EU_NO@index)
+
+HERAS_baseline <- list(HERAS.NSAS=HERAS.NSAS,
+                       HERAS.WBSS=HERAS.WBSS,
+                       stk.WBSS=WBSS,
+                       stk.NSAS=NSAS)
+
+save(HERAS_baseline,
+     file = file.path("model",'HERAS_baseline.RData'))
--- output.R
+++ output.R
@@ -1,23 +1,59 @@
 ## Extract results of interest, write TAF output tables
 library(icesTAF)
+taf.library(FLFleet)
+taf.library(FLCore)
 
 mkdir("output")
 
-data_mapping <- read.csv("bootstrap/data/data_mapping.csv")
-project_years <- unique(data_mapping$year)
+# load project mapping
+project_mapping <- read.csv("bootstrap/data/project_mapping.csv")
 
-for(idxYear in project_years){
-  currentProjects <- as.character(unique(data_mapping[data_mapping$year == idxYear,]$project))
+load(file.path("model",'HERAS_baseline.RData'))
 
-  for(idxProject in currentProjects){
-    msg("creating outout for: ", idxProject)
-    load(file.path("model", paste0(idxProject, "_BaseLineReport.RData")))
+# write NSAS index at age
+write.taf(flr2taf(HERAS_baseline$HERAS.NSAS$HERAS_all@index), 
+          file = "NSAS_indexAtAge.csv",
+          dir="output")
+# write NSAS SSB
+write.taf(flr2taf(HERAS_baseline$stk.NSAS@stock), 
+          file = "NSAS_SSB.csv",
+          dir="output")
+# write NSAS proportion mature
+write.taf(flr2taf(HERAS_baseline$stk.NSAS@mat), 
+          file = "NSAS_mat.csv",
+          dir="output")
+# write NSAS weight at age
+write.taf(flr2taf(HERAS_baseline$stk.NSAS@stock.wt), 
+          file = "NSAS_wa.csv",
+          dir="output")
 
-    # test output creation
-    write.taf(
-      as.data.frame(currentBaseLineReport$outputData$FillMissingData),
-      file = paste0(idxProject, "FillMissingData.csv"),
-      dir = "output"
-    )
-  }
+
+
+# write WBSS index at age
+write.taf(flr2taf(HERAS_baseline$HERAS.WBSS$HERAS_all@index), 
+          file = "WBSS_indexAtAge.csv",
+          dir="output")
+# write WBSS SSB
+write.taf(flr2taf(HERAS_baseline$stk.WBSS@stock), 
+          file = "WBSS_SSB.csv",
+          dir="output")
+# write WBSS proportion mature
+write.taf(flr2taf(HERAS_baseline$stk.WBSS@mat), 
+          file = "WBSS_mat.csv",
+          dir="output")
+# write WBSS weight at age
+write.taf(flr2taf(HERAS_baseline$stk.NSAS@stock.wt), 
+          file = "WBSS_wa.csv",
+          dir="output")
+
+
+# some example of StoX output (superIndividual table)
+for(idxProject in 1:dim(project_mapping)[1]){
+  msg("creating outout for: ", project_mapping$project_name[idxProject])
+  load(file.path("model", paste0('BaseLineReport_',
+                                 project_mapping$project_name[idxProject], ".RData")))
+  write.taf(
+    as.data.frame(currentBaseLineReport$outputData$FillMissingData),
+    file = paste0(idxProject, "FillMissingData.csv"),
+    dir = "output")
 }
